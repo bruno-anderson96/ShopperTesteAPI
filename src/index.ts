3268bfb262f46191d093e4b6bf9c720986dbb979
Bruno <brunoanderson.macedorosendo@gmail.com>
2024-08-27 17:43:30 -0300

    uuid + saving on DB via Postman

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,78 @@
 import 'dotenv/config';
 import express from 'express';
 import { query } from './database';
+import { v4 as uuidv4 } from 'uuid';
 
 const geminiApiKey = process.env.GEMINI_API_KEY;
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 
+type Record = {
+  uuid: string,
+  image: string,
+  customer_code: string,
+  measure_datetime: Date;
+  measure_type: "WATER" | "GAS" ;
+}
+
 async function uploadImg(){
 
 // Access your API key as an environment variable
-const genAI = new GoogleGenerativeAI(geminiApiKey as string);
+  const genAI = new GoogleGenerativeAI(geminiApiKey as string);
 
-const fileManager = new GoogleAIFileManager(geminiApiKey as string);
+  const fileManager = new GoogleAIFileManager(geminiApiKey as string);
 
-const uploadResponse = await fileManager.uploadFile("src/img/medidor2.jpg", {
-  mimeType: "image/jpeg",
-  displayName: "drawing",
-});
+  const uploadResponse = await fileManager.uploadFile("src/img/medidor2.jpg", {
+    mimeType: "image/jpeg",
+    displayName: "Medidor",
+  });
 
-console.log(`Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`);
+  console.log(`Uploaded file ${uploadResponse.file.displayName} as: ${uploadResponse.file.uri}`);
 
-const getResponse = await fileManager.getFile(uploadResponse.file.name);
+  const getResponse = await fileManager.getFile(uploadResponse.file.name);
 
-console.log(`Retrieved file ${getResponse.displayName} as ${getResponse.uri}`);
+  console.log(`Retrieved file ${getResponse.displayName} as ${getResponse.uri}`);
 
 //
-const model = genAI.getGenerativeModel({
+  const model = genAI.getGenerativeModel({
   // Choose a Gemini model.
-  model: "gemini-1.5-pro",
-});
+    model: "gemini-1.5-pro",
+  });
 
 // Upload file ...
 
 // Generate content using text and the URI reference for the uploaded file.
-const result = await model.generateContent([
-    {
-      fileData: {
-        mimeType: uploadResponse.file.mimeType,
-        fileUri: uploadResponse.file.uri
-      }
-    },
-    { text: "Nessa imagem está medindo o consumo de que ? e qual valor ?" },
+  const result = await model.generateContent([
+  {
+    fileData: {
+      mimeType: uploadResponse.file.mimeType,
+      fileUri: uploadResponse.file.uri
+    }
+  },
+  { text: "Nessa imagem está medindo o consumo de que ? e qual valor ?" },
   ]);
 
 // Output the generated text to the console
-console.log(result.response.text())
+  console.log(result.response.text())
 };
 
+function isBase64(str: string): boolean {
+  const base64Pattern = /^[A-Za-z0-9+/=]+$/;
+  return base64Pattern.test(str) && str.length % 4 === 0;
+}
 
-uploadImg();
+function isValidMeasureType(type: string): type is "WATER" | "GAS" {
+  const UpperFormat = type.toUpperCase();
+  return type === "WATER" || type === "GAS";
+}
+
+function generateUUID(): string {
+  return uuidv4();
+}
+
+
+// uploadImg();
 
 
 const app = express();
@@ -62,7 +85,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   // Define o código de status 200 e envia uma resposta JSON personalizada
   res.status(200).json({
-    message: 'Hello World',
+    message: 'Hello Shopper',
     timestamp: new Date().toISOString()
   });
 
@@ -90,7 +113,6 @@ app.get('/teste', (req, res) => {
 });
 
 app.get('/records', async (req, res) => {
-  // Define o código de status 200 e envia uma resposta JSON personalizada
   try{
     const results = await query("Select * from data_record");
     res.status(200).json(results);
@@ -100,7 +122,53 @@ app.get('/records', async (req, res) => {
 
 });
 
-// Inicia o servidor
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.post('/upload', async (req, res) => {
+  const data: Record = req.body;
+//, customer_code, measure_datetime, measure_type
+  data.measure_datetime = new Date();
+  console.log(data)
+  if(!isBase64(data.image) || (data.customer_code == null) || (!data.measure_type)){
+    return res.status(400).json({
+      error: 'INVALID_DATA',
+      message: "Os dados fornecidos no corpo da requisição são inválidos"
+    });
+  }
+
+  //Checa se já existe no banco
+    const rows = await query(
+      `Select * from data_record where measure_type = ? and img = ?`,
+      [data.measure_type, data.image]
+      );
+    if((rows as any[]).length === 0){
+      data.uuid = generateUUID();
+      await query(
+        'INSERT INTO data_record(id, measure_type, img, measure_datetime, customer_code) VALUES (?,?,?,?,?)',
+        [data.uuid, data.measure_type, data.image, data.measure_datetime, data.customer_code]
+        );
+
+      res.status(200).json({
+        message: 'Novo registro de medição adicionado com sucesso',
+        timestamp: new Date().toISOString()
+      });
+    }else{
+      return res.status(409).json({
+      error: 'DOUBLE_REPORT',
+      message: "Leitura do mês já realizada"
+    
+  })
+    }
+ 
+
+  //Adicionar aqui:
+  // {"image_url" : string,
+  // "measure_value" : integer,
+  // "measure_uuid" : string
+  // }
+  
+
 });
+
+// Inicia o servidor
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
